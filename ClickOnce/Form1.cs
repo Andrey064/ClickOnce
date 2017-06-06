3618dc07b497f5ab741f4d7598a6b7890d3647bb
Andrey064 <pankinau@mail.ru>
2017-06-06 15:35:59 +0400

    тест windows installer

--- ClickOnce/Form1.cs
+++ ClickOnce/Form1.cs
@@ -16,5 +16,10 @@ namespace ClickOnce
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("My First ClickOnce Application");
+        }
     }
 }
